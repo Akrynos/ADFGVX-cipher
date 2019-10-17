eca0d0cb4b83c40aa725e4aeba09c7b30b11a80c
Grachu <grachu9099@gmail.com>
2019-10-17 23:09:57 +0200

    This is some kind of unfunny joke

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
@@ -15,10 +17,8 @@ public class Main extends Application
 {
     public static void main(String[] args)
     {
-
         Application.launch(args);
-        //cypher.getCodeAndFileName();
-        //cypher.determineArrayRows();
+
 
         //long startTime = System.currentTimeMillis();
 
@@ -35,31 +35,73 @@ public class Main extends Application
     public void start(Stage primaryStage) throws Exception {
         ADFGVX cypher = new ADFGVX();
 
-        primaryStage.setTitle("My First JavaFX App");
+        primaryStage.setTitle("Cypher app");
 
-        Label insertKey = new Label("Insert key here:   ");
-        Label encrypt = new Label("Hello World, JavaFX !");
-        Label decrypt = new Label("Hello World, JavaFX !");
-        Label KEY = new Label("Hello World, JavaFX !");
+        Label insertKey = new Label("Insert key here:");
+        Label insertText = new Label("Insert text here:");
+        Label insertedKey = new Label("Inserted key:");
+        Label result = new Label("Result:");
+        Label encrypt = new Label("Characters encription:");
+        Label decrypt = new Label("Encription to characters:");
+        Label enc = new Label(" ");
+        Label dec = new Label(" ");
+        Label KEY = new Label("");
+        TextField keyTextField = new TextField();
         TextField textField = new TextField();
-        textField.setText("ADME");
-        Button button = new Button("My Button");
+        TextField res = new TextField();
+        keyTextField.setText("ADME");
+        textField.setText("SupeRTestowanko");
+        Button gen = new Button("Generate table");
+        Button encr = new Button("Encryption");
+        Button decr = new Button("Decryption");
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
+        //Button generating tables
+        gen.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
                 cypher.loadEncryptValues();
-                encrypt.setText(cypher.returnEncryptHashMap());
+                enc.setText(cypher.returnEncryptHashMap());
+                enc.setWrapText(true);
                 cypher.loadDecryptValues();
-                decrypt.setText(cypher.returnDecryptHashMap());
-                KEY.setText(textField.getText());
+                dec.setText(cypher.returnDecryptHashMap());
+                dec.setWrapText(true);
+                KEY.setText(keyTextField.getText());
+                if(KEY.getText().length()%2 != 0){
+                    res.setText("Blad! Wprowadz klucz o parzystej ilosci znakow!");
+                } else cypher.getKey(KEY.getText());
             }
         });
 
-        HBox hbox = new HBox( textField, button);
-        VBox vboxhbox = new VBox(insertKey, hbox);
-        vboxhbox.setStyle("-fx-background-color: #abc; -fx-padding: 15px; -fg-margin: 10px; -fx-font-size: 20;");
-        VBox vbox = new VBox(vboxhbox, KEY, encrypt, decrypt);
-        Scene scene = new Scene(vbox, 800, 300);
+        //Button encrypting text
+        encr.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                cypher.determineArrayRows(textField.getText());
+                cypher.encryptData();
+            }
+        });
+
+        //Button decrypting text
+        decr.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                cypher.determineArrayRows(textField.getText());
+            }
+        });
+        HBox buttons = new HBox(gen, encr, decr );
+        buttons.setSpacing(5);
+        HBox key = new HBox(insertKey, keyTextField);
+        key.setSpacing(5);
+        HBox text = new HBox(insertText, textField);
+        text.setSpacing(5);
+        HBox results = new HBox(result, res);
+        HBox ki = new HBox(insertedKey, KEY);
+        VBox tableData = new VBox(encrypt, enc, decrypt, dec);
+        VBox programData = new VBox(key,text, ki, results);
+        programData.setSpacing(5);
+        VBox container = new VBox(programData, tableData,buttons);
+        container.setStyle("-fx-background-color: #abc; -fx-padding: 15px; -fx-font-size: 14;");
+        enc.setStyle("-fx-font-size: 12;");
+        dec.setStyle("-fx-font-size: 12;");
+        buttons.setStyle("-fx-padding-top: 10; -fx-margin-top: 10;");
+        Scene scene = new Scene(container, 800, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
